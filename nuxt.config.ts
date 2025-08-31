f5d99dfce43ef9530b6403d690d262312d8ca2c5
Gábor Soós <soos.gabor86@gmail.com>
2025-08-31 20:26:22 +0200

    feat: add lint (#48)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt/config";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/eslint"],
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+});
