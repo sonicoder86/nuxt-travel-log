7e58e9c053551f40ffeea48f77b2ae6fe151733b
Gábor Soós <soos.gabor86@gmail.com>
2025-09-07 18:10:16 +0200

    TL-11 homepage (#50)
    * feat: tailwindcss and icons
    
    * feat: new lockfile
    
    * feat: eslint tailwindcss
    
    * feat: homepage

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,5 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <button class="btn btn-primary">
-    Primary <Icon name="tabler:map" />
-  </button>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
