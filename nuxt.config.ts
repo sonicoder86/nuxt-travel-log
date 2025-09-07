c1b8f7bcdc8c17b6df34ac3da56f53d9a6d56eb3
Gábor Soós <soos.gabor86@gmail.com>
2025-09-07 16:13:34 +0200

    TL-10: tailwindcss and icons (#49)
    * feat: tailwindcss and icons
    
    * feat: new lockfile
    
    * feat: eslint tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
