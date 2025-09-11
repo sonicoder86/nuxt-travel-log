e22c2d38c4456ac83abb5a566a949c932ced841c
Gábor Soós <soos.gabor86@gmail.com>
2025-09-11 20:21:50 +0200

    feat: add env schema validation (#52)

---
+++ app/lib/env.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+import tryParseEnv from "./try-parse-env";
+
+const envSchema = z.object({
+  NODE_ENV: z.string(),
+});
+
+export type EnvSchema = z.infer<typeof envSchema>;
+
+// eslint-disable-next-line node/no-process-env
+export default tryParseEnv(envSchema, process.env);
