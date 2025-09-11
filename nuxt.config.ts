81752d90babb0a44f340f0668e164243a3ce8f20
Gabor Soos <soos.gabor86@gmail.com>
2025-09-11 19:31:39 +0200

    feat: theme toggle

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,11 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxtjs/color-mode",
+  ],
   eslint: {
     config: {
       standalone: false,
@@ -13,5 +17,8 @@ export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
+  },
+  colorMode: {
+    dataValue: "theme",
   },
 });
