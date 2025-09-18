52a6ee6ee5ababe58479274ebbbfb86799e3e096
Gabor Soos <soos.gabor86@gmail.com>
2025-09-18 19:55:04 +0200

    TL-14: authentication setup

--- app/lib/db/schema/location-log-image.ts
+++ app/lib/db/schema/location-log-image.ts
@@ -1,11 +1,13 @@
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+import { user } from "./auth";
 import { locationLog } from "./location-log";
 
 export const locationLogImage = sqliteTable("location_log_image", {
   id: int().primaryKey({ autoIncrement: true }),
   key: text().notNull(),
   locationLogId: int().notNull().references(() => locationLog.id, { onDelete: "cascade" }),
+  userId: int().notNull().references(() => user.id, { onDelete: "cascade" }),
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
 });
