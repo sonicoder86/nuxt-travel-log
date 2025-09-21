44bca842f86200fc9ba4af188a59e0bacf1c943e
Gábor Soós <soos.gabor86@gmail.com>
2025-09-21 16:34:29 +0200

    TL-17: logout (#58)
    * TL-17: logout
    
    * TL-17: style fix

--- app/components/app/nav-bar.vue
+++ app/components/app/nav-bar.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="navbar bg-primary text-primary-content">
-    <div class="navbar-start">
+  <div class="navbar bg-primary">
+    <div class="navbar-start text-primary-content">
       <NuxtLink to="/" class="btn btn-ghost text-xl">
         Travel Log
       </NuxtLink>
     </div>
     <div class="navbar-end">
-      <AppThemeToggle />
+      <AppThemeToggle class="text-primary-content" />
       <AuthButton />
     </div>
   </div>
