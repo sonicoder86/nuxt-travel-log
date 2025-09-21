eeef44ed442a3c9add8ba8d0c5b4e055b0a2579c
Gabor Soos <soos.gabor86@gmail.com>
2025-09-21 16:27:55 +0200

    TL-17: logout

--- app/stores/auth.ts
+++ app/stores/auth.ts
@@ -1,19 +1,24 @@
-import { createAuthClient } from "better-auth/client";
+import { createAuthClient } from "better-auth/vue";
 
 const authClient = createAuthClient();
 
 export const useAuthStore = defineStore("authStore", () => {
-  const loading = ref(false);
+  const session = authClient.useSession();
+  const user = computed(() => session.value?.data?.user);
+  const loading = computed(() => session.value?.isPending || session.value?.isRefetching);
 
   async function signIn() {
-    loading.value = true;
     await authClient.signIn.social({
       provider: "github",
       callbackURL: "/dashboard",
       errorCallbackURL: "/error",
     });
-    loading.value = false;
   }
 
-  return { loading, signIn };
+  async function signOut() {
+    await authClient.signOut();
+    navigateTo("/");
+  }
+
+  return { loading, signIn, signOut, user };
 });
