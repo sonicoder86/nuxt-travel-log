c4eb75febb2ade06f4bfe5b4e892bd8f76f82e68
Gabor Soos <soos.gabor86@gmail.com>
2025-10-18 15:57:24 +0200

    TL-24: location name chacks

---
+++ app/lib/db/queries/location.ts
@@ -0,0 +1,38 @@
+import { and, eq } from "drizzle-orm";
+import { customAlphabet } from "nanoid";
+import { alphanumeric } from "nanoid-dictionary";
+
+import type { InsertLocationType } from "~/lib/db/schema";
+
+import db from "~/lib/db";
+import { location } from "~/lib/db/schema";
+
+export async function findLocationByName(existing: InsertLocationType, userId: number) {
+  return db.query.location.findFirst({
+    where: and(
+      eq(location.name, existing.name),
+      eq(location.userId, userId),
+    ),
+  });
+}
+
+export async function findLocationBySlug(slug: string) {
+  return db.query.location.findFirst({
+    where: eq(location.slug, slug),
+  });
+}
+
+export async function findUniqueSlug(slug: string) {
+  let existing = !!(await findLocationBySlug(slug));
+  const nanoid = customAlphabet(alphanumeric, 5);
+
+  while (existing) {
+    const idSlug = `${slug}-${nanoid()}`;
+    existing = !!(await findLocationBySlug(idSlug));
+    if (!existing) {
+      return idSlug;
+    }
+  }
+
+  return slug;
+}
