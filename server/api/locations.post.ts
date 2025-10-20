57df9019b2e508789a5c62707b570de6bcdba08a
Gabor Soos <soos.gabor86@gmail.com>
2025-10-20 08:42:50 +0200

    feat: list locations

--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -5,15 +5,9 @@ import slugify from "slug";
 import { findLocationByName, findUniqueSlug, insertLocation } from "~/lib/db/queries/location";
 import { InsertLocation } from "~/lib/db/schema";
 import sendZodError from "~/lib/send-zod-error";
+import defineAuthenticatedEventHandler from "~/utils/define-authenticated-event-handler";
 
-export default defineEventHandler(async (event) => {
-  if (!event.context.user) {
-    throw createError({
-      statusCode: 401,
-      statusMessage: "Unauthorized",
-    });
-  }
-
+export default defineAuthenticatedEventHandler(async (event) => {
   const result = await readValidatedBody(event, InsertLocation.safeParse);
 
   if (!result.success) {
